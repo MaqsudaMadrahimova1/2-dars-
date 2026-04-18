9c413ab7895766965940cfaebb439a39f9886f41
maqsudamadrahimova.9@gmail.com <maqsudamadrahimova.9@gmail.com>
2026-04-18 19:13:07 +0500

    task

--- src/auth/model/auth.entity.ts
+++ src/auth/model/auth.entity.ts
@@ -1,6 +1,35 @@
-import { Model } from "sequelize";
+import { DataTypes } from "sequelize";
+import { Column, Model, Table } from "sequelize-typescript";
 
+@Table
 export class Auth extends Model {
-    
+ @Column({
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false
+ })
+ declare id: number;
+ @Column({
+    type: DataTypes.STRING,
+    allowNull: false,
+ })
+   username: string;
+    @Column({
+        type: DataTypes.STRING,
+        allowNull: false,
+ } )
+ email!: string;
+    @Column({
+        type: DataTypes.STRING,
+        allowNull: false,
+    })
+    password: string;
+
+    @Column({   
+       type: DataTypes.STRING,
+         allowNull: false,
+ } ) 
+    otp!: string;
 }
 
